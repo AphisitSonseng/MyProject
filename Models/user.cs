fcf366d386f0a56247b569049b4cf7375d8f5a30
AphisitSonseng <635021000594@mail.rmutk.ac.th>
2023-05-01 20:01:46 +0700

    addmodel

---
+++ Models/user.cs
@@ -0,0 +1,10 @@
+namespace cmpos{
+    public class user
+    {
+        public int id {get; set; }
+        public string fullname {get; set; }
+        public string username {get; set; }
+        public string password {get; set; }
+        public string email {get; set; }
+    }
+}
